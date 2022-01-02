d6fd7c0424572432d0e286211c3fc8338765836b
Kevin <94573335+kev1n999@users.noreply.github.com>
2022-01-02 14:37:09 -0300

    Create avatar.js

---
+++ Example/Commands/Utils/avatar.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "avatar",
+    description: "Retorna o avatar do usuário mencionado",
+    options: [
+        {
+            name: "usuário",
+            description: "Mencione o usuário para ver o avatar",
+            type: "USER",
+            required: true
+        }
+    ],
+
+    run: async (client, interaction) => {
+        const { commandName, options } = interaction;
+
+        if (commandName === "avatar") {
+            const user = options.getUser("usuário");
+            const userAvatar = user.displayAvatarURL({ format: "png", size: 1024 });
+
+            const avatarEmbed = new MessageEmbed()
+
+            .setTitle(`:frame_photo: Avatar de ${user.username}`)
+            .setDescription(`Clique [aqui](${userAvatar}) para baixar a imagem!`)
+            .setImage(userAvatar)
+            .setColor("RED")
+
+            await interaction.reply({ embeds: [avatarEmbed] });
+        }
+    }
+}
